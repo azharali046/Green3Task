2b069ee302a3cf810ecf2ff998ed77a8486b8f93
Azhar Ali <ali@youattest.com>
2023-04-14 11:31:57 +0500

    new directory changes

--- tests/config_test.py
+++ tests/config_test.py
@@ -21,6 +21,8 @@ engine = create_engine(
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
+# Set up the database once
+Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
 
